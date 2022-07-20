62005b3ef2980aeddcb2858c8944dae1bf334756
Christy Le <christylex3@gmail.com>
2022-07-19 22:46:46 -0400

    Created engineerQuestions array

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const inquirer = require('inquirer');
 
 // An array of questions that asks about the team manager
 const teamManagerQuestions = [
-    "What is your team manager's name?",
-    "What is your team manager's employee ID?",
-    "What is your team manager's email?",
-    "What is your team manager's office number?"
-]
+    "What is the team manager's name?",
+    "What is the team manager's employee ID?",
+    "What is the team manager's email?",
+    "What is the team manager's office number?"
+];
+
+// An array of questions that asks about the engineer
+const engineerQuestions = [
+    "What is the engineer's name?",
+    "What is the engineer's employee ID?",
+    "What is the engineer's email?",
+    "What is the engineer's GitHub username?"
+];
