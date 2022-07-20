a7cf9e5d6824c2530d462e1e88780589c8be0b5f
Christy Le <christylex3@gmail.com>
2022-07-19 22:41:31 -0400

    Imported fs and inquirer. Created teamManagerQuestions array

---
+++ index.js
@@ -0,0 +1,11 @@
+// Packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// An array of questions that asks about the team manager
+const teamManagerQuestions = [
+    "What is your team manager's name?",
+    "What is your team manager's employee ID?",
+    "What is your team manager's email?",
+    "What is your team manager's office number?"
+]
