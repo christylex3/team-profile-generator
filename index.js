56331be5672c7fb4c1d7b5190283f4cc2eb6c09d
Christy Le <christylex3@gmail.com>
2022-07-21 00:16:27 -0400

    Remade teamManagerQuestions, engineerQuestions, and internQuestions. Then added memberQuestion

--- index.js
+++ index.js
@@ -4,68 +4,102 @@ const inquirer = require('inquirer');
 
 // An array of questions that asks about the team manager
 const teamManagerQuestions = [
-    "What is the team manager's name?",
-    "What is the team manager's employee ID?",
-    "What is the team manager's email?",
-    "What is the team manager's office number?"
+    {
+        type: 'input',
+        message: "What is the team manager's name?",
+        name: 'teamManagerName',
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's employee ID?",
+        name: 'teamManagerID'
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's email?",
+        name: 'teamManagerEmail'
+    },
+    {
+        type: 'input',
+        message: "What is the team manager's office number?",
+        name: 'teamManagerOfficeNum'
+    },
 ];
 
 // An array of questions that asks about the engineer
 const engineerQuestions = [
-    "What is the engineer's name?",
-    "What is the engineer's employee ID?",
-    "What is the engineer's email?",
-    "What is the engineer's GitHub username?"
+    {
+        type: 'input',
+        message: "What is the engineer's name?",
+        name: 'engineerName',
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's employee ID?",
+        name: 'engineerID'
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's email?",
+        name: 'engineerEmail'
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's GitHub username?",
+        name: 'engineerGitHub'
+    },
 ];
 
 // An array of questions that asks about the intern
 const internQuestions = [
-    "What is the intern's name?",
-    "What is the intern's employee ID?",
-    "What is the intern's email?",
-    "What school does the intern attend?"
+    {
+        type: 'input',
+        message: "What is the intern's name?",
+        name: 'internName',
+    },
+    {
+        type: 'input',
+        message: "What is the intern's employee ID?",
+        name: 'internID'
+    },
+    {
+        type: 'input',
+        message: "What is the intern's email?",
+        name: 'internEmail'
+    },
+    {
+        type: 'input',
+        message: "What school does the intern attend?",
+        name: 'internSchool'
+    },
 ];
+
+const memberQuestion = [
+    {
+        type: 'list',
+        message: "Would you like to add another team member?",
+        name: 'userChoice',
+        choices: ['Engineer', 'Intern', 'No, that is all'],
+    }
+];
+
 
 // Initializes app
 function init() {
     inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: teamManagerQuestions[0],
-                name: 'teamManagerName',
-            },
-            {
-                type: 'input',
-                message: teamManagerQuestions[1],
-                name: 'teamManagerID'
-            },
-            {
-                type: 'input',
-                message: teamManagerQuestions[2],
-                name: 'teamManagerEmail'
-            },
-            {
-                type: 'input',
-                message: teamManagerQuestions[3],
-                name: 'teamManagerOfficeNum'
-            },
-        ])
+        .prompt(teamManagerQuestions)
         .then(recallInquirer())
+        .then((data) => {
+            console.log(data);
+        });
 };
 
+// Helper method - lets engineeer and intern questions become recursive
 function recallInquirer() {
     inquirer
-        .prompt([
-            {
-                type: 'list',
-                message: "Would you like to add another team member?",
-                name: 'userChoice',
-                choices: ['Engineer', 'Intern', 'No, that is all'],
-            }
-        ])
+        .prompt(memberQuestion)
         .then((data) => {
-             if (data.userChoice == "Engineer") {
+            if (data.userChoice == "Engineer") {
                 // method of inquirer for engineer
                 // then recall this method
             } else if (data.userChoice == "Intern") {
