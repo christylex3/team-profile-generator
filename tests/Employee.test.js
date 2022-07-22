d782b12102bc71969dafcc9f2185e023a2039289
Christy Le <christylex3@gmail.com>
2022-07-21 23:06:46 -0400

    Enable npm test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,9 +1,12 @@
 const Employee = require('../lib/Employee');
 
 describe("Employee", () => {
-    it("Can make a new instance of Employee", () => {
-        const person = new Employee();
+    describe("Instantiation", () => {
+        it("Can make a new instance of Employee", () => {
+            const person = new Employee();
 
-        expect("Employee" in person).toEqual(true);
+            expect(person instanceof Employee).toEqual(true);
+        });
+
     });
-})
+});
