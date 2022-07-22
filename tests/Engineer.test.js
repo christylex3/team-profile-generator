bff8cc31b583f0b53657b9b0faf5572258d8c40a
Christy Le <christylex3@gmail.com>
2022-07-22 00:21:01 -0400

    Finished writing unit test for Engineer

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -2,10 +2,20 @@ const Engineer = require('../lib/Engineer');
 
 describe("Engineer", () => {
     it("Can make a new instance of Enginner", () => {
-        let person = new Engineer();
+        const person = new Engineer();
 
         expect(person instanceof Engineer).toEqual(true);
     });
 
-    
+    it("Can set the github username of the Engineer through constructor", () => {
+        const person = new Engineer("Sun", 77, "sun@gmail.com", "thegreatsunGitHub");
+
+        expect(person.github == "thegreatsunGitHub").toEqual(true);
+    });
+
+    it("Can get the role of the Engineer through getRole()", () => {
+        const person = new Engineer("Sun", 77, "sun@gmail.com", "thegreatsunGitHub");
+
+        expect(person.getRole() == "Engineer").toEqual(true);
+    });
 });
