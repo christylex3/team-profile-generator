3808a0ea9779588fb103d062a33f7d229572e3a2
Christy Le <christylex3@gmail.com>
2022-07-22 00:56:13 -0400

    Fixed Intern constructor and finished writing unit test for intern

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,21 @@
 const Intern = require('../lib/Intern');
+
+describe("Intern", () => {
+    it("Can make a new instance of Intern", () => {
+        const person = new Intern();
+
+        expect(person instanceof Intern).toEqual(true);
+    });
+
+    it("Can get the Intern's school through getSchool()", () => {
+        const person = new Intern("Moon", 27, "Moon@gmail.com", "GT");
+
+        expect(person.school == "GT").toEqual(true);
+    });
+
+    it("Can get the Intern's role through getRole()", () => {
+        const person = new Intern("Moon", 27, "Moon@gmail.com", "GT");
+
+        expect(person.getRole() == "Intern").toEqual(true);
+    });
+});
