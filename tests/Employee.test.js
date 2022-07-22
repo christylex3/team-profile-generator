383e9b44787976eeaf6e67f35f1e63938ec7f36e
Christy Le <christylex3@gmail.com>
2022-07-21 23:48:20 -0400

    Setting up unit test for setting name, id, and email for Employee through constructor

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,12 +1,29 @@
 const Employee = require('../lib/Employee');
 
 describe("Employee", () => {
-    describe("Instantiation", () => {
-        it("Can make a new instance of Employee", () => {
-            const person = new Employee();
+    it("Can make a new instance of Employee", () => {
+        let person = new Employee();
 
-            expect(person instanceof Employee).toEqual(true);
-        });
-
+        expect(person instanceof Employee).toEqual(true);
     });
+
+    it("Can set the name of the Employee through constructor", () => {
+        let person = new Employee("Mo", 11, "mo@gmail.com");
+
+        expect(person.name === "Mo").toEqual(true);
+    });
+
+    it("Can set the id of the Employee through constructor", () => {
+        const person = new Employee("Mo", 11, "mo@gmail.com");
+
+        expect(person.id == "11").toEqual(true);
+    });
+
+    it("Can set the email of the Employee through constructor", () => {
+        const person = new Employee("Mo", 11, "mo@gmail.com");
+
+        expect(person.email == "mo@gmail.com").toEqual(true);
+    });
+
+    
 });
