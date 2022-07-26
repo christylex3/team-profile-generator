8547edda8f6eacbfee224455615b1f18f4b82a5a
Christy Le <christylex3@gmail.com>
2022-07-25 20:56:22 -0400

    Added comments to the methods to explain their purpose

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ var engineerPerson;
 var internPerson;
 
 // Arrays to hold data for employees
-// var managerArray = [];
 var engineerArray = [];
 var internArray = [];
 
@@ -107,7 +106,6 @@ function startQuestions() {
     inquirer
         .prompt(teamManagerQuestions)
         .then((teamManagerData) => {
-            console.log(teamManagerData);
             managerPerson = new Manager(
                 teamManagerData.managerName,
                 teamManagerData.managerId,
@@ -133,7 +131,6 @@ function recallInquirer() {
                 inquirer
                     .prompt(engineerQuestions)
                     .then((engineerData) => {
-                        console.log(engineerData);
                         engineerPerson = new Engineer(
                             engineerData.engineerName,
                             engineerData.engineerId,
@@ -150,7 +147,6 @@ function recallInquirer() {
                 inquirer
                     .prompt(internQuestions)
                     .then((internData) => {
-                        console.log(internData);
                         internPerson = new Intern(
                             internData.internName,
                             internData.internId,
@@ -162,8 +158,6 @@ function recallInquirer() {
                     });
 
             } else {
-                console.log(data);
-                console.log("Test, test");
                 // method that writes the HTML file with Bootstrap
                 writeToFile("teamProfile.html");
 
@@ -176,13 +170,10 @@ function writeToFile(fileName) {
     fs.writeFile(`./src/${fileName}`, fileContents, (err) =>
         err ? console.error(err) : console.log("The team profile has been updated.")
     );
-    // need to send the file into src
-
 }
 
+// Generates the Team Profile (HTML file) based on the user's input
 function generateHTMLFile() {
-    // add in the basics of HTML
-    // use bootstrap to make cards
     return `${renderHtmlHead()}<body>${renderHtmlBody()}${renderHtmlCards()}</body>\n</html>`;
 
 }
@@ -192,16 +183,18 @@ function renderHtmlHead() {
     return `<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0" />\n\t<meta http-equiv="X-UA-Compatible" content="ie=edge" />\n\t<title>Team Profile</title>\n\t<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">\n</head>\n\n`;
 }
 
+// Helper method - adds some body part of the HTML and uses Bootstrap
 function renderHtmlBody() {
     return `\n<header class="d-flex justify-content-center bg-dark">\n\t<h1 class="text-white p-5">My Team</h1>\n</header>\n<main>\n\t<section class="d-flex flex-row justify-content-center flex-wrap">\n`;
 }
 
+// Generates the employees' cards based on user's input
 function renderHtmlCards() {
     // Manager card
     var managerCard = `<article class="m-5 border shadow-lg">\n\t<section class="p-3 bg-primary text-white">\n\t<h2>${managerPerson.getName()}</h2>\n\t<article class="d-flex">\n\t<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"\n\tclass="mr-2 bi bi-cup-hot-fill align-self-center" viewBox="0 0 16 16">\n<path fill-rule="evenodd"\n\td="M.5 6a.5.5 0 0 0-.488.608l1.652 7.434A2.5 2.5 0 0 0 4.104 16h5.792a2.5 2.5 0 0 0 2.44-1.958l.131-.59a3 3 0 0 0 1.3-5.854l.221-.99A.5.5 0 0 0 13.5 6H.5ZM13 12.5a2.01 2.01 0 0 1-.316-.025l.867-3.898A2.001 2.001 0 0 1 13 12.5Z" />\n<path
 d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 3.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 3.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 3 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 4.4.8Zm3 0-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.31 3.31 0 0 1-.202.388 5.444 5.444 0 0 1-.253.382l-.018.025-.005.008-.002.002A.5.5 0 0 1 6.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 6.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 6 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 7.4.8Zm3 0-.003.004-.014.019a4.077 4.077 0 0 0-.204.31 2.337 2.337 0 0 0-.141.267c-.026.06-.034.092-.037.103v.004a.593.593 0 0 0 .091.248c.075.133.178.272.308.445l.01.012c.118.158.26.347.37.543.112.2.22.455.22.745 0 .188-.065.368-.119.494a3.198 3.198 0 0 1-.202.388 5.385 5.385 0 0 1-.252.382l-.019.025-.005.008-.002.002A.5.5 0 0 1 9.6 4.2l.003-.004.014-.019a4.149 4.149 0 0 0 .204-.31 2.06 2.06 0 0 0 .141-.267c.026-.06.034-.092.037-.103a.593.593 0 0 0-.09-.252A4.334 4.334 0 0 0 9.6 2.8l-.01-.012a5.099 5.099 0 0 1-.37-.543A1.53 1.53 0 0 1 9 1.5c0-.188.065-.368.119-.494.059-.138.134-.274.202-.388a5.446 5.446 0 0 1 .253-.382l.025-.035A.5.5 0 0 1 10.4.8Z" />\n</svg>\n<h3>${managerPerson.getRole()}</h3>\n</article>\n</section>\n<section class="p-3 bg-light d-flex flex-column">\n\t<p class="p-2 bg-white border">ID: ${managerPerson.getId()}</p>\n<p class="p-2 bg-white border">Email: <a href="mailto:${managerPerson.getEmail()}">${managerPerson.getEmail()}</a></p>\n<p class="p-2 bg-white border">Office Number: ${managerPerson.getOfficeNumber()}</p>\n</section>\n</article>`;
 
-    // make a for-loop that goes through the array of intern and engineer
+    // Generates the engineer card by grabbing data from user's input
     var engineerCard = ``;
     for (let i = 0; i < engineerArray.length; i++) {
         engineerCard += `<article class="m-5 border shadow-lg">
@@ -224,6 +217,7 @@ d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267
     </article>`
     }
 
+    // Generates the intern card by grabbing data from user's input
     var internCard = ``;
     for (let i = 0; i < internArray.length; i++) {
         internCard += `<article class="m-5 border shadow-lg">
@@ -246,9 +240,9 @@ d="m4.4.8-.003.004-.014.019a4.167 4.167 0 0 0-.204.31 2.327 2.327 0 0 0-.141.267
         </section>
         </article>`
     }
+    // returns all cards together
     return `${managerCard}\n${engineerCard}\n${internCard}`;
 }
-
 
 // Calls startQuestions()
 startQuestions();
